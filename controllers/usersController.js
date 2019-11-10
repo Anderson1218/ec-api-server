3cc946dcaa9a3aea5ef8e5ff745e54f77e2b2f5f
Anderson <anderson771218@gmail.com>
2019-11-10 16:05:27 +0800

    code refactoring in usersController

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,8 +1,7 @@
 const UsersRef = require("../models/Users");
-const LoginInfosRef = require("../models/LoginInfos");
-const db = require("../firebase/firebase-admin");
 const bcrypt = require("bcryptjs");
 const jwtUtils = require("../utils/jwt-utils");
+const omit = require("lodash/omit");
 
 exports.signup = async (req, res) => {
   try {
@@ -12,24 +11,16 @@ exports.signup = async (req, res) => {
     }
     let snapshot = await UsersRef.where("email", "==", email).get();
     if (snapshot.empty) {
-      const batch = db.batch();
       const hash = await bcrypt.hash(password, 8);
       const createdAt = new Date();
-      let newUser = {
+      const newUser = {
         email,
         name,
         createdAt,
-        photoURL
+        photoURL,
+        password: hash
       };
-      let newUserLoginInfo = {
-        email,
-        hash
-      };
-      const userRef = UsersRef.doc();
-      const LoginInfoRef = LoginInfosRef.doc();
-      batch.set(userRef, newUser);
-      batch.set(LoginInfoRef, newUserLoginInfo);
-      await batch.commit();
+      await UsersRef.add(newUser);
       return res.json("signup success");
     } else {
       throw new Error("email has already been used");
@@ -45,22 +36,21 @@ exports.signin = async (req, res) => {
     if (!email || !password) {
       throw new Error("email and password shouldn't be empty");
     }
-    let snapshot = await LoginInfosRef.where("email", "==", email)
+    let querySnapshot = await UsersRef.where("email", "==", email)
       .limit(1)
       .get();
-    if (snapshot.empty) {
+    if (querySnapshot.empty) {
       throw new Error("the user doesn't exist!");
     } else {
-      let userLoginInfo = snapshot.docs[0].data();
-      const isMatch = await bcrypt.compare(password, userLoginInfo.hash);
+      const userSnapshot = querySnapshot.docs[0];
+      const user = userSnapshot.data();
+      const isMatch = await bcrypt.compare(password, user.password);
       if (!isMatch) {
         throw new Error("the password is wrong");
       }
-      const usersSnapshot = await UsersRef.where("email", "==", email).get();
-      const userSnapshot = usersSnapshot.docs[0];
-      const user = userSnapshot.data();
       const token = await jwtUtils.generateAuthToken(userSnapshot.id);
-      return res.json({ user, token });
+      const userProfile = omit(user, ["password"]);
+      return res.json({ user: userProfile, token });
     }
   } catch (error) {
     return res.status(400).send(error.message);
